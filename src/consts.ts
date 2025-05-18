6b65dffbf9e7592e3928ae4100b9d1abaef8cbf1
Frank Sasto <sastoman@gmail.com>
2025-05-17 17:05:31 -0700

    A bunch of shit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Beyond the Bag';
+export const SITE_DESCRIPTION =
+  'The journey from homemade pasta to a national snack brand. America’s sexiest little snack food.';
